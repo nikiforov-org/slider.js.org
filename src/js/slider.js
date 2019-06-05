2b3bb8665aeea958512c9539792432c19eb445e8
nikiforov-org <e@nikiforov.org>
2019-06-05 15:33:03 +0300

    modify swipe

--- src/js/slider.js
+++ src/js/slider.js
@@ -161,63 +161,31 @@
         }
 
         /**
-         * Loop slider
+         * Swipe slider
          */
         if (swipe) {
             (function(slider) {
                 var 
-                    touchStartCoords =  {'x':-1, 'y':-1}, // X and Y coordinates on mousedown or touchstart events.
-                    touchEndCoords = {'x':-1, 'y':-1},// X and Y coordinates on mouseup or touchend events.
-                    direction = 'undefined',// Swipe direction
-                    minDistanceXAxis = 30,// Min distance on mousemove or touchmove on the X axis
-                    maxDistanceYAxis = 30,// Max distance on mousemove or touchmove on the Y axis
-                    maxAllowedTime = 1000,// Max allowed time between swipeStart and swipeEnd
-                    startTime = 0,// Time on swipeStart
-                    elapsedTime = 0;// Elapsed time between swipeStart and swipeEnd
+                    firstX, 
+                    lastX;
 
-                function swipeStart(e) {
-                    e = e ? e : window.event;
-                    e = ('changedTouches' in e)?e.changedTouches[0] : e;
-                    touchStartCoords = {'x':e.pageX, 'y':e.pageY};
-                    startTime = new Date().getTime();
-                }    
-
-                function swipeMove(e){
-                    e = e ? e : window.event;
-                    e.preventDefault();
-                }
-
-                function swipeEnd(e) {
-                    e = e ? e : window.event;
-                    e = ('changedTouches' in e)?e.changedTouches[0] : e;
-                    touchEndCoords = {'x':e.pageX - touchStartCoords.x, 'y':e.pageY - touchStartCoords.y};
-                    elapsedTime = new Date().getTime() - startTime;
-                    if (elapsedTime <= maxAllowedTime){
-                        if (Math.abs(touchEndCoords.x) >= minDistanceXAxis && Math.abs(touchEndCoords.y) <= maxDistanceYAxis){
-                        direction = (touchEndCoords.x < 0)? 'left' : 'right';
-                        switch(direction) {
-                            case 'left':
-                                slideTo(slider, 'next', pager);;
-                                break;
-                            case 'right':
-                                slideTo(slider, 'prev', pager);
-                                break;
-                            }
-                        }
+                slider.addEventListener('touchstart', function(e){
+                    var touch = e.touches[0];
+                    firstX = touch.pageX;
+                });
+                    
+                slider.addEventListener('touchmove', function(e) {
+                    var touch = e.touches[0];
+                    lastX = touch.pageX;
+                });
+        
+                slider.addEventListener('touchend', function() {
+                    if (firstX < lastX - 50) {
+                        slideTo(slider, 'prev', pager);
+                    } else if (firstX > lastX + 50) {
+                        slideTo(slider, 'next', pager);
                     }
-                }
-
-                function addMultipleListeners(el, s, fn) {
-                    var evts = s.split(' ');
-                    for (var i = 0, iLen= evts.length; i < iLen; i++) {
-                        el.addEventListener(evts[i], fn, false);
-                    }
-                }
-
-                addMultipleListeners(slider, 'mousedown touchstart', swipeStart);
-                addMultipleListeners(slider, 'mousemove touchmove', swipeMove);
-                addMultipleListeners(slider, 'mouseup touchend', swipeEnd);
-
+                });
             })(slider);
         }
     }
